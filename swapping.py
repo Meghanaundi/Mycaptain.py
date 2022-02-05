7a377fdd78b4d5ed48bc5f5f189f6361bd9bde0c
Meghanaundi <97959286+Meghanaundi@users.noreply.github.com>
2022-02-05 21:34:46 +0530

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -2,13 +2,13 @@
 int main()
 {
  int a=10,b=20;
- printf("Before swap a=%d b=%d \n",a,b);
+ printf("Before swapping a=%d b=%d \n",a,b);
  a=a+b;
  b=a-b;
  a=a-b;
- printf("After swap a=%d b=%d \n",a,b);
+ printf("After swapping a=%d b=%d \n",a,b);
  return 0;
 }
 /*output
-Before swap a=10 b=20 
-After swap a=20 b=10 */ 
+Before swapping a=10 b=20 
+After swapping a=20 b=10 */ 
